356acfc6c54b7aac0d13183f02439e2b36ef441f
hakan0814 <h.k.cuhadar@gmail.com>
2020-01-09 22:12:55 +0100

    Add contact icons and contact text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,11 @@ export default function About() {
   return (
     <div>
       <h4 style={{ marginTop: '100px' }}>Portfolio Website - About page</h4>
+      <p>
+        Write a paragraph about myself, explaining in short my background (culturally and
+        interests), my experiences, my achievements, what am I proud of. Talk about my hobbies,
+        interests, and things I like. <strong>Be you and informally introduce yourself.</strong>
+      </p>
     </div>
   );
 }
--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -4,6 +4,11 @@ export default function Experience() {
   return (
     <div>
       <h4 style={{ marginTop: '100px' }}>Portfolio Website - Experience page</h4>
+      <p>
+        Write short paragraph including work experience, education experience, and life experience.
+        Value should reflect personality, work ethics, and intelligence. Explaine why I would be
+        suitable for companies and developer positions
+      </p>
     </div>
   );
 }
