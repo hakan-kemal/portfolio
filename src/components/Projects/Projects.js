9c665b2440e70ffd2ad120ae69f65d897cc5dc69
hakan0814 <h.k.cuhadar@gmail.com>
2020-01-11 11:59:03 +0100

    Add project links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import { LogoGithub } from 'styled-icons/octicons/LogoGithub';
+import { Globe } from 'styled-icons/boxicons-regular/Globe';
+
+const LogoGithubIcon = styled(LogoGithub)`
+  color: #2d3436;
+  height: 50px;
+  margin: 0 10px;
+`;
+
+const GlobeIcon = styled(Globe)`
+  color: #2d3436;
+  height: 50px;
+  margin: 0 10px;
+`;
 
 export default function Projects() {
   return (
@@ -6,6 +21,45 @@ export default function Projects() {
       <h4 style={{ marginTop: '100px' }}>
         Projects page - Designed and coded by Hakan Çuhadar, 2020. All rights reserved.
       </h4>
+      <a
+        href='https://github.com/hakan-kemal/space-explorer-client'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <LogoGithubIcon />
+      </a>
+      <a
+        href='https://github.com/hakan-kemal/game-client'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <LogoGithubIcon />
+      </a>
+      <a
+        href='https://github.com/hakan-kemal/pre-academy-homework'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <LogoGithubIcon />
+      </a>
+      <br />
+      <a href='https://api.spacexdata.com/v3/launches' target='_blank' rel='noopener noreferrer'>
+        <GlobeIcon />
+      </a>
+      <a
+        href='https://obscure-beach-71010.herokuapp.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <GlobeIcon />
+      </a>
+      <a
+        href='https://pre-academy-website-hakan.netlify.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <GlobeIcon />
+      </a>
       <ul>
         <li>Hackathon project; must be updated and improved actually</li>
         <li>Real world project; leave as is</li>
